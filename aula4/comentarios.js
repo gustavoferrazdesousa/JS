c329fc8a833e4f9805b142311c44707f5c0c1938
gustavoferrazdesousa <gustavoferrazdesousa@gmail.com>
2024-03-25 22:13:02 -0300

    Corrigir bug de formatação no código

---
+++ aula4/comentarios.js
@@ -0,0 +1,8 @@
+// Isso é um comentario javascript de linha única
+/* Isso é um comentario javascript de multiplas linhas, tudo que está dentro dele é ignorado pelo interpretador */
+
+console.log('Olá mundo!'); // Isso é um console.log que imprime "Olá mundo!" no console do navegador.
+console.log('linha 1');
+// console.log('linha 2'); 
+/*console.log('linha 3');
+console.log('linha 4'); */
